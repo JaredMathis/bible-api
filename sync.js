5fdab6c03cc8bea95ad79b7fb7857f3d5cecdf37
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-07-18 13:15:34 -0400

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,5 +1,9 @@
 const u = require('wlj-utilities');
 
-const command = `aws s3 sync ./api s3://bible-api`;
-const output = u.executeCommand(command);
-console.log(output);
+const books = require('./api/translation/kjv/books.json');
+books.forEach(book => {
+    const p = `/translation/kjv/book/${book}`;
+    const command = `aws s3 sync ./api${p} s3://bible-api${p}`;
+    const output = u.executeCommand(command);
+    console.log(output);
+});
