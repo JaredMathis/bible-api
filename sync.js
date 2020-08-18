bd7838642b1b5f6309166bdbd96e86954b82ad79
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-08-18 19:47:27 -0400

    Update sync.js

--- sync.js
+++ sync.js
@@ -7,3 +7,10 @@ books.forEach(book => {
     const output = u.executeCommand(command);
     console.log(output);
 });
+
+(() => {
+    const p = `/translation/kjv/books.json`;
+    const command = `aws s3 cp ./api${p} s3://bible-api${p}`;
+    const output = u.executeCommand(command);
+    console.log(output);
+})();
