84be27da4ae5ae8dec117cc72b896fa06522e5b1
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-08-29 12:00:06 -0400

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,16 +1,23 @@
 const u = require('wlj-utilities');
 
-const books = require('./api/translation/kjv/books.json');
-books.forEach(book => {
-    const p = `/translation/kjv/book/${book}`;
-    const command = `aws s3 sync ./api${p} s3://bible-api${p}`;
-    const output = u.executeCommand(command);
-    console.log(output);
-});
+const versions = [
+    'nkjv',
+    'kjv',
+];
 
-(() => {
-    const p = `/translation/kjv/books.json`;
-    const command = `aws s3 cp ./api${p} s3://bible-api${p}`;
-    const output = u.executeCommand(command);
-    console.log(output);
-})();
+versions.forEach(version => {
+    const books = require(`./api/translation/${version}/books.json`);
+    books.forEach(book => {
+        const p = `/translation/${version}/book/${book}`;
+        const command = `aws s3 sync ./api${p} s3://bible-api${p}`;
+        const output = u.executeCommand(command);
+        console.log(output);
+    });
+    
+    (() => {
+        const p = `/translation/${version}/books.json`;
+        const command = `aws s3 cp ./api${p} s3://bible-api${p}`;
+        const output = u.executeCommand(command);
+        console.log(output);
+    })();
+})
